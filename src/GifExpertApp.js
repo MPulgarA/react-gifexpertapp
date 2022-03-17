ca5cdb6e946d02a7a31f42bf942bd4c53f7c05f7
MPulgarA <matiaspulgaralarcon@gmail.com>
2022-03-16 22:38:56 -0300

    Comunicación entre componentes

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,24 +1,16 @@
 import React, { useState } from 'react'
+import AddCategory from './components/AddCategory';
 
 export const GifExpertApp = () => {
 
-    // A menos que hagamos una lista que no reciba más parametros y no se necesite renderizar nuevamente, esto no se hace
-    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
     const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
-
-    const handleAdd = () => {
-        setCategories([...categories, 'Jujutsu Kaisen']);
-        // Callback del useState en categories
-        // setCategories(c => [...categories, 'Jujutsu Kaisen']);
-    };
 
     return (
         <>
             <h1>GifExpertApp</h1>
+            <AddCategory setCategories={setCategories}/>
             <hr />
-            <button onClick={handleAdd}>Agregar</button>
             <ol>
-                {/* No debemos colocar el valor de key con el indice, ya que es muy volatil */}
                 {
                     categories.map(c => {
                         return <li key={c}>{c}</li>;
