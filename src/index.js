7bb83a4cbf68e0ca03c3329f6bb56b4d3302518a
MPulgarA <matiaspulgaralarcon@gmail.com>
2022-03-16 21:04:07 -0300

    Lista de categories

--- src/index.js
+++ src/index.js
@@ -6,5 +6,3 @@ import {GifExpertApp} from './GifExpertApp';
 
 ReactDOM.render(<GifExpertApp />, document.getElementById('root'));
 
-
-// dentro del src crear componentee GifExpertApp, con un h2 de titulo y un hr
